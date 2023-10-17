6f442dda9ca09b1c28cc0a5b4e85cfd007a1d4e3
adriana.p <adriana.p@SABRLBP7NS53>
2023-10-17 20:40:39 -0300

    Add project files.

---
+++ ControleDeContatos/Controllers/ContatoController.cs
@@ -0,0 +1,61 @@
+﻿using ControleDeContatos.Models;
+using ControleDeContatos.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        private readonly IContatoRepositorio _contatoRepositorio;
+
+        public ContatoController(IContatoRepositorio contatoRepositorio) 
+        {
+            _contatoRepositorio = contatoRepositorio;
+        }
+        public IActionResult Index()
+        {
+            List<ContatoModel> contatos = _contatoRepositorio.BuscarTodos();
+            return View(contatos);
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar(int id)
+        {
+            ContatoModel contato = _contatoRepositorio.ListarPorId(id);
+            return View(contato);
+        }
+
+        public IActionResult ApagarConfirmacao(int id)
+        {
+            ContatoModel contato = _contatoRepositorio.ListarPorId(id);
+            return View(contato);
+        }
+                
+        public IActionResult Apagar(int id)
+        {
+            _contatoRepositorio.Apagar(id);
+            return RedirectToAction("Index"); // voltar para a página index
+        }
+
+        [HttpPost]
+        public IActionResult Criar(ContatoModel contato)
+        {
+            if (ModelState.IsValid) // se a informação do ModelState é válida (validações dos campos da tela)
+            {
+                _contatoRepositorio.Adicionar(contato);  // adiciona o contato no banco de dados
+                return RedirectToAction("Index"); // voltar para a página index
+            }            
+            return View(contato);  // se não for válida retorna para a view passando o objeto contato
+        }
+        [HttpPost]
+        public IActionResult Alterar(ContatoModel contato)
+        {
+            _contatoRepositorio.Atualizar(contato);  // adiciona o contato no banco de dados
+            return RedirectToAction("Index"); // voltar para a página index
+        }
+    }
+}
---
+++ ControleDeContatos/Repositorio/ContatoRepositorio.cs
@@ -0,0 +1,59 @@
+﻿using ControleDeContatos.Data;
+using ControleDeContatos.Models;
+
+namespace ControleDeContatos.Repositorio;
+
+public class ContatoRepositorio : IContatoRepositorio
+{
+    private readonly BancoContext _bancoContext;
+    public ContatoRepositorio(BancoContext bancoContext)
+    {
+        _bancoContext = bancoContext;
+    }
+
+    public ContatoModel ListarPorId(int id)
+    {
+        return _bancoContext.Contatos.FirstOrDefault(x => x.Id == id);
+    }
+
+    public List<ContatoModel> BuscarTodos()
+    {
+        return _bancoContext.Contatos.ToList(); // traz todos os dados do banco e lista
+    }
+
+    public ContatoModel Adicionar(ContatoModel contato)
+    {
+        // gravar no banco de dados
+        _bancoContext.Contatos.Add(contato);  // Contatos se refere a classe BancoContext
+        _bancoContext.SaveChanges();          // confirma gravação no banco de dados
+        return contato;
+    }
+
+    public ContatoModel Atualizar(ContatoModel contato)
+    {
+        ContatoModel contatoDB = ListarPorId(contato.Id);
+
+        if (contatoDB == null) throw new Exception("Houve um erro na atualização do contato!");
+
+        contatoDB.Nome = contato.Nome;
+        contatoDB.Email = contato.Email;
+        contatoDB.Celular = contato.Celular;
+
+        _bancoContext.Contatos.Update(contatoDB);
+        _bancoContext.SaveChanges();
+
+        return contatoDB;
+    }
+
+    public bool Apagar(int id)
+    {
+        ContatoModel contatoDB = ListarPorId(id);
+
+        if (contatoDB == null) throw new Exception("Houve um erro na deleção do contato!");
+
+        _bancoContext.Contatos.Remove(contatoDB);
+        _bancoContext.SaveChanges();
+
+        return true;
+    }
+}
